1742c0a22ac0b391fd61ada2846b35bf703a9fa1
Joao <joao@appsimplantadores.com>
2024-10-31 17:37:47 +0100

    clase1

---
+++ clase1/ejercicio1.js
@@ -0,0 +1,19 @@
+
+
+
+let numero = prompt('Digite um numero')
+
+numero = parseInt(numero);
+
+
+
+if (!isNaN(numero)) {
+    if (numero % 2  === 0) {
+        console.log('o numero é par');
+    } else {
+        console.log('o numero é impar');
+    } }
+    
+    else {
+        console.log('Digite um numero')
+    }
